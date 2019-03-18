63202322d63d71443f511d3bdf075f3c91eb9893
Christian Kleinhuis <ck@froso.de>
2019-03-18 11:35:49 +0100

    missing

---
+++ src/app/view/components/App.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import TodoModule from '../../../todos/view/components/material_ui/TodoModule'
+import TodoModuleReact from '../../../todos/view/components/react_only/TodoModule'
+
+const App = () => (<div style={{
+        display: 'flex'
+    }} >
+        <div   style={{
+            width: '50%'
+        }}>
+
+            <TodoModule />
+        </div >
+        <div style={{
+            width: '50%'
+        }} >
+            <TodoModuleReact />
+        </div >
+    </div >
+)
+
+export default App
